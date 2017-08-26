e95049e867c5dd96b6de7988d4788fa501b702b0
WouldChar <wouldchar@gmail.com>
2017-08-26 14:34:40 +0800

    cnss: Bring in ztemt changes

--- drivers/net/wireless/cnss/cnss_pci.c
+++ drivers/net/wireless/cnss/cnss_pci.c
@@ -99,9 +99,20 @@ static struct cnss_fw_files FW_FILES_QCA6174_FW_1_3 = {
 static struct cnss_fw_files FW_FILES_QCA6174_FW_3_0 = {
 "qwlan30.bin", "bdwlan30.bin", "otp30.bin", "utf30.bin",
 "utfbd30.bin", "epping30.bin", "evicted30.bin"};
+#ifdef CONFIG_MACH_ZTEMT_MSM8994
+static struct cnss_fw_files FW_FILES_QCA6174_FW_3_EPCOS = {
+"qwlan30.bin", "bdwlan3e.bin", "otp30.bin", "utf30.bin",
+"utfbd30.bin", "epping30.bin", "evicted30.bin"};
+#endif
 static struct cnss_fw_files FW_FILES_DEFAULT = {
 "qwlan.bin", "bdwlan.bin", "otp.bin", "utf.bin",
 "utfbd.bin", "epping.bin", "evicted.bin"};
+
+#ifdef CONFIG_MACH_ZTEMT_MSM8994
+extern const char* ztemt_get_hw_wifi(void);
+#define SAMSUNG_WIFI_FRONT_END_VERSION "wifi_samsung"
+#define EPCOS_WIFI_FRONT_END_VERSION "wifi_epcos"
+#endif
 
 #define QCA6180_VENDOR_ID	(0x168C)
 #define QCA6180_DEVICE_ID      (0x0041)
@@ -786,6 +797,11 @@ static u8 cnss_get_pci_dev_bus_number(struct pci_dev *pdev)
 
 void cnss_setup_fw_files(u16 revision)
 {
+#ifdef CONFIG_MACH_ZTEMT_MSM8994
+	const char *wifi_front_end;
+	wifi_front_end = ztemt_get_hw_wifi();
+#endif
+
 	switch (revision) {
 
 	case QCA6174_FW_1_1:
@@ -831,8 +847,22 @@ void cnss_setup_fw_files(u16 revision)
 	case QCA6174_FW_3_2:
 		strlcpy(penv->fw_files.image_file, "qwlan30.bin",
 			CNSS_MAX_FILE_NAME);
+
+#ifdef CONFIG_MACH_ZTEMT_MSM8994
+		if(strncmp(SAMSUNG_WIFI_FRONT_END_VERSION, wifi_front_end, strlen(wifi_front_end)) == 0)
+			strlcpy(penv->fw_files.board_data, "bdwlan30.bin",
+				CNSS_MAX_FILE_NAME);
+		else if(strncmp(EPCOS_WIFI_FRONT_END_VERSION, wifi_front_end, strlen(wifi_front_end)) == 0)
+			strlcpy(penv->fw_files.board_data, "bdwlan3e.bin",
+				CNSS_MAX_FILE_NAME);
+		else
+			strlcpy(penv->fw_files.board_data, "bdwlan30.bin",
+				CNSS_MAX_FILE_NAME);
+
+#else
 		strlcpy(penv->fw_files.board_data, "bdwlan30.bin",
 			CNSS_MAX_FILE_NAME);
+#endif
 		strlcpy(penv->fw_files.otp_data, "otp30.bin",
 			CNSS_MAX_FILE_NAME);
 		strlcpy(penv->fw_files.utf_file, "utf30.bin",
@@ -870,8 +900,16 @@ EXPORT_SYMBOL(cnss_get_fw_files);
 int cnss_get_fw_files_for_target(struct cnss_fw_files *pfw_files,
 					u32 target_type, u32 target_version)
 {
+#ifdef CONFIG_MACH_ZTEMT_MSM8994
+	const char *wifi_front_end;
+#endif
+
 	if (!pfw_files)
 		return -ENODEV;
+
+#ifdef CONFIG_MACH_ZTEMT_MSM8994
+	wifi_front_end = ztemt_get_hw_wifi();
+#endif
 
 	switch (target_version) {
 	case AR6320_REV1_VERSION:
@@ -886,7 +924,25 @@ int cnss_get_fw_files_for_target(struct cnss_fw_files *pfw_files,
 		break;
 	case AR6320_REV3_VERSION:
 	case AR6320_REV3_2_VERSION:
+#ifdef CONFIG_MACH_ZTEMT_MSM8994
+		if(strncmp(SAMSUNG_WIFI_FRONT_END_VERSION, wifi_front_end, strlen(wifi_front_end)) == 0)
+		{
+			memcpy(pfw_files, &FW_FILES_QCA6174_FW_3_0, sizeof(*pfw_files));
+			pr_info("%s: Download samsung RF bin file successful\n", __func__);
+		}
+		else if(strncmp(EPCOS_WIFI_FRONT_END_VERSION, wifi_front_end, strlen(wifi_front_end)) == 0)
+		{
+			memcpy(pfw_files, &FW_FILES_QCA6174_FW_3_EPCOS, sizeof(*pfw_files));
+			pr_info("%s: Download epcos RF bin file successful\n", __func__);
+		}
+		else
+		{
+		    memcpy(pfw_files, &FW_FILES_QCA6174_FW_3_0, sizeof(*pfw_files));
+			pr_err("%s: Download unkown RF bin file successful, %s \n", __func__, wifi_front_end);
+		}
+#else
 		memcpy(pfw_files, &FW_FILES_QCA6174_FW_3_0, sizeof(*pfw_files));
+#endif
 		break;
 	default:
 		memcpy(pfw_files, &FW_FILES_DEFAULT, sizeof(*pfw_files));
